10efcb137a62c0798802a7f7fa9abd5f474488b0
Richard Berthold <5139@ctucareer.co.za>
2019-08-16 00:16:20 +0200

    Upload to github

---
+++ Assets/Script/PausedMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resuming game...");
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void SaveMenu()
+    {
+        Debug.Log("Game is saved");
+        Time.timeScale = 1f;
+        PauseMenuUI.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+}
