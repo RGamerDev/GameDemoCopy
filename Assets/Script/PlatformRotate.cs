c7a9697a07be8ccc1a01f902de0feb13b05f1d97
Richard Berthold <5139@ctucareer.co.za>
2019-09-03 22:40:19 +0200

    Level Designed

---
+++ Assets/Script/PlatformRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRotate : MonoBehaviour
+{
+    public GameObject Platforms;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3 { x = 0f, y = 0f, z = 1f});
+    }
+}
